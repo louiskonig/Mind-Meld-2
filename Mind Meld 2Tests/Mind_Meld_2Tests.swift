e1320e0296d08bafcf24e74100a05c5140805fb7
louiskonig <louiskonig@gmail.com>
2015-01-01 12:25:45 -0500

    Initial Commit

---
+++ Mind Meld 2Tests/Mind_Meld_2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Mind_Meld_2Tests.swift
+//  Mind Meld 2Tests
+//
+//  Created by Louis Konig on 1/1/15.
+//  Copyright (c) 2015 Louis Konig. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Mind_Meld_2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
